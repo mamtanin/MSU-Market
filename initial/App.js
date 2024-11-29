8c5058b6feae4f691a8ca292e98aae49fac787e3
Nikhil Mamtani <mamtanin@msu.edu>
2024-11-29 16:46:39 -0600

    Login Screen

--- initial/App.js
+++ initial/App.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import Login from "./app/screens/Login";
 
-//GO TO THIS LINK TO FIND API FOR ALL THESE THINGS LIKE BUTTON, IMAGE WHATEVER https://reactnative.dev/docs/appregistry
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ViewImage"
+          component={ViewImageScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- initial/app/screens/ViewImageScreen.js
+++ initial/app/screens/ViewImageScreen.js
@@ -1,40 +1,35 @@
-import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, View, Alert } from "react-native";
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}></View>
       <View style={styles.deleteIcon}></View>
-      <Image
-        resizeMode="contain"
-        style={styles.image}
-        source={require("../assets/icon.png")}
-      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightgrey",
     flex: 1,
   },
   closeIcon: {
-    width: 50, // Fixed typo here
-    height: 50, // Fixed typo here
-    backgroundColor: "#fc5c65",
-    position: "absolute",
-    top: 40,
-    left: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 350,
+    height: 400,
+    backgroundColor: "#fff",
+    borderRadius: 20,
   },
+
   deleteIcon: {
     width: 50, // Fixed typo here
     height: 50, // Fixed typo here
-    backgroundColor: "#4ecdc4",
-    position: "absolute",
-    top: 40,
-    right: 30,
+    backgroundColor: "royalblue",
   },
   image: {
     width: "100%",
