642f62b14038421a61bff6b2d9482d5d4a9273d0
Nikhil Mamtani <mamtanin@msu.edu>
2025-01-22 19:41:49 -0500

    cccc

--- initial/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-
-//GO TO THIS LINK TO FIND API FOR ALL THESE THINGS LIKE BUTTON, IMAGE WHATEVER https://reactnative.dev/docs/appregistry
-export default function App() {
-  return <WelcomeScreen />;
-}
